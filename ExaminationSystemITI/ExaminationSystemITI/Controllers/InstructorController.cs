87d5b820d62198417f37f4d9f7d50c262334c3d8
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-03-10 13:08:04 +0200

    compelete curd on instructor

--- ExaminationSystemITI/ExaminationSystemITI/Controllers/InstructorController.cs
+++ ExaminationSystemITI/ExaminationSystemITI/Controllers/InstructorController.cs
@@ -1,19 +1,58 @@
 ﻿using ExaminationSystemITI.Abstractions.Interfaces;
+using ExaminationSystemITI.Database;
+using ExaminationSystemITI.Models.Tables;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ExaminationSystemITI.Controllers
 {
     public class InstructorController : Controller
     {
         IInstructorService _instructor;
+        
         public InstructorController(IInstructorService instructor)
         {
             _instructor = instructor;
+            
         }
         public IActionResult GetIns()
         {
             var instructors = _instructor.GetInstructors();
             return View(instructors);
+        }
+
+        [HttpGet]
+        public IActionResult AddIns()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddIns(Instructor instructor)
+        {
+            _instructor.InsertInstructor(instructor);
+            return RedirectToAction("GetIns");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var instructor = _instructor.FindInstructor(id);
+            return View(instructor);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Instructor ins)
+        {
+            _instructor.EditInstructor(ins);
+            return RedirectToAction("Getins");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Instructor ins)
+        {
+            _instructor.DeleteInstructor(ins.ID);
+            return RedirectToAction("Getins");
         }
     }
 }
