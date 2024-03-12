e5247b1e49bfcc9f56ee0ee69447bb5e0e55418a
Ahmed <ahamdy01@yahoo.com>
2024-03-12 16:35:26 +0200

    Department

---
+++ ExaminationSystemITI/ExaminationSystemITI/Abstractions/Interfaces/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using ExaminationSystemITI.Models.Tables;
+
+namespace ExaminationSystemITI.Abstractions.Interfaces
+{
+    public interface IDepartmentService
+    {
+        public List<Department> GetDepartments();
+        public void InsertDepartment(Department dep);
+        public void EditDepartment(Department dep);
+        public void DeleteDepartment(int depID);
+        public Department FindDepartment(int id);
+    }
+}
---
+++ ExaminationSystemITI/ExaminationSystemITI/Controllers/DepartmentController.cs
@@ -0,0 +1,57 @@
+﻿using ExaminationSystemITI.Abstractions.Interfaces;
+using ExaminationSystemITI.Models.Tables;
+using ExaminationSystemITI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExaminationSystemITI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        IDepartmentService _department;
+        public DepartmentController(IDepartmentService department)
+        { 
+           _department = department;
+        }
+
+        public IActionResult GetDep()
+        {
+            var departments = _department.GetDepartments();
+            return View(departments);
+        }
+        [HttpGet]
+        public IActionResult AddDep()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDep(Department department)
+        {
+            _department.InsertDepartment(department);
+            return RedirectToAction("GetDep");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var department = _department.FindDepartment(id);
+            return View(department);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Department dep)
+        {
+            _department.EditDepartment(dep);
+            return RedirectToAction("GetDep");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Department dep)
+        {
+            _department.DeleteDepartment(dep.Id);
+            return RedirectToAction("GetDep");
+        }
+
+
+    }
+}
