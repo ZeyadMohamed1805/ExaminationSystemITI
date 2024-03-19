3873047da1a3d779857300838590f436ca054ad9
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-03-20 00:58:38 +0200

    more Enhance in(view of active exams for st/view of exams-questions for ins)

---
+++ ExaminationSystemITI/ExaminationSystemITI/Models/ViewModels/ExamCourses.cs
@@ -0,0 +1,12 @@
+﻿using ExaminationSystemITI.Models.Tables;
+
+namespace ExaminationSystemITI.Models.ViewModels
+{  
+    //for instructor in exam controller
+    public class ExamCourses
+    {
+        public List<Exam> Exams { get; set; }
+        public List<Course> Courses { get; set; }
+
+    }
+}
